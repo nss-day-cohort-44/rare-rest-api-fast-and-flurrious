7ae572a90710872093e1b6d77d29434ea39b8a4a
Heath Lester <h.allen.lester@gmail.com>
2021-02-18 14:59:43 -0600

    added views, models, pull request template seed file and fixtures folders

---
+++ seed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf djangorarapi/migrations
+rm db.sqlite3
+python3 manage.py migrate
+python3 manage.py makemigrations djangorarapi
+python3 manage.py migrate djangorarapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata posts
+python3 manage.py loaddata comments
+python3 manage.py loaddata categories
+python3 manage.py loaddata tags
+python3 manage.py loaddata post_comments
+python3 manage.py loaddata post_tags
