425f1726d6439abdcc763e255240a84634494dee
Heath Lester <h.allen.lester@gmail.com>
2021-02-22 09:50:23 -0600

    resolved merge conflicts

--- djangoRare/urls.py
+++ djangoRare/urls.py
@@ -1,19 +1,17 @@
-from djangorarapi.views import Posts, Categories
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from djangorarapi.views import register_user, login_user
 from djangorarapi.views import Comments
+from djangorarapi.views import Posts, Categories, Comments, Tags
 
-from djangorarapi.views import Tags
 
-from djangorarapi.views import Posts, Categories
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'comments', Comments, 'comment')
-router.register(r"tags", Tags, "tag")
 router.register(r'categories', Categories, 'category')
+router.register(r'comments', Comments, 'comment')
 router.register(r'posts', Posts, 'post')
+router.register(r"tags", Tags, "tag")
 
 urlpatterns = [
     path('', include(router.urls)),
