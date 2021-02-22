55b2663c0b84f162ddc3ba3edc03a76b3f8afb42
KyleSimmonsC44 <darrensimmons92@gmail.com>
2021-02-22 09:48:45 -0600

    destroy and retrieve written

--- djangorarapi/views/post.py
+++ djangorarapi/views/post.py
@@ -45,6 +45,41 @@ class Posts(ViewSet):
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single game
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            post = Post.objects.get(pk=pk)
+            post.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Post.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single game
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        try:
+            # `pk` is a parameter to this function, and
+            # Django parses it from the URL route parameter
+            #   http://localhost:8000/games/2
+            #
+            # The `2` at the end of the route becomes `pk`
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(post, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
 
 
 class PostSerializer(serializers.ModelSerializer):
